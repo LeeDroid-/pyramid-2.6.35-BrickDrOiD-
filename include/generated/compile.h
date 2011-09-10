fda017585d7335eb179bcc3019451b3ffebe3f22
showp1984 <showp1984@gmail.com>
2011-09-10 07:35:28 +0200

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Fri Sep 9 10:31:18 CEST 2011"
+#define LINUX_COMPILE_TIME "10:31:18"
+#define LINUX_COMPILE_BY "dex"
+#define LINUX_COMPILE_HOST "developer"
+#define LINUX_COMPILE_DOMAIN "MyOwnNet"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.13-Bricked-0.2-opt-cifs-utf8-fwifi-1.5-192.show-p1984"

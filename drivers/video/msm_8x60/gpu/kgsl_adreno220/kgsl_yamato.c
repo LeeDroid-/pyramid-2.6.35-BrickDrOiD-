fa62cecb2bb33a270464bc06ad7d8531d68aa892
showp1984 <showp1984@gmail.com>
2011-10-26 22:17:36 +0200

    drivers: video: kgsl: readded ringbuffer poweroverrides, rewrote for better performance

--- drivers/video/msm_8x60/gpu/kgsl_adreno220/kgsl_yamato.c
+++ drivers/video/msm_8x60/gpu/kgsl_adreno220/kgsl_yamato.c
@@ -793,6 +793,7 @@ static int kgsl_yamato_start(struct kgsl_device *device, unsigned int init_ram)
 	int status = -EINVAL;
 	struct kgsl_yamato_device *yamato_device = KGSL_YAMATO_DEVICE(device);
 	int init_reftimestamp = 0x7fffffff;
+	unsigned int override1, override2, i;
 
 	KGSL_DRV_VDBG("enter (device=%p)\n", device);
 
@@ -858,10 +859,31 @@ static int kgsl_yamato_start(struct kgsl_device *device, unsigned int init_ram)
 	kgsl_yamato_regwrite(device, REG_SQ_VS_PROGRAM, 0x00000000);
 	kgsl_yamato_regwrite(device, REG_SQ_PS_PROGRAM, 0x00000000);
 
-	if (device->chip_id != KGSL_CHIPID_LEIA_REV470) {
+	if (device->chip_id != KGSL_CHIPID_LEIA_REV470){
 		kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE1, 0);
-		kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE2, 0x80);
+		kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE2, 0);
 	}
+	else{
+		/* This was rewrote for better performance
+		 * by show-p1984 <showp1984@gmail.com>
+		 * try writing override1 & 2, cancel if successful
+		 * max three times.
+		 */
+		i = 0;
+		while((((override1 & 0x7BFFFFFA) != 0x7BFFFFFA) &&
+				((override2 & 0x000001F4) != 0x000001F4)) && i < 3){
+			kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE1, 0x7BFFFFFA);
+			kgsl_yamato_regread(device, REG_RBBM_PM_OVERRIDE1, &override1);
+			kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE2, 0x000001F4);
+			kgsl_yamato_regread(device, REG_RBBM_PM_OVERRIDE2, &override2);
+			i++;
+			if (i==2) {
+				if (((override1 & 0x7BFFFFFA) != 0x7BFFFFFA) &&
+					((override2 & 0x000001F4) != 0x000001F4))
+				KGSL_DRV_ERR("OVERRIDE1 = 0x%x, OVERRIDE2 = 0x%x !!\n",	override1, override2);
+			}
+		}
+        }
 
 	kgsl_sharedmem_set(&device->memstore, 0, 0,
 			   device->memstore.size);
